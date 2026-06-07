bec5306d77393c6112a03322bbc554d2c1565f18
anooppraturu <anooppraturu@gmail.com>
2026-06-07 10:33:48 -0700

    added periodic + reflecting BC to convolutional layer. Built layernorm2d layer, but after some experiments sticking with batchnorm2d. refactored architectures into layers and models

---
+++ src/tyndall/layers.py
@@ -0,0 +1,108 @@
+from torch import nn
+import torch.nn.functional as F
+
+class LayerNorm2D(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.ln = nn.LayerNorm(channels)
+
+    def forward(self, x):
+        #input (B, C, H, W)
+        #permute to (B, H, W, C) since layernorm acts on last dimension
+        x = x.permute(0, 2, 3, 1).contiguous()
+        x = self.ln(x)
+        # permute back
+        x = x.permute(0, 3, 1, 2).contiguous()
+
+        return x
+
+
+class MixedPeriodicConv2D(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, lat_pad_mode="reflect"):
+        super().__init__()
+
+        self.lat_pad_mode = lat_pad_mode
+        self.kernel_size = kernel_size
+        # preserves spatial size when stride = 1
+        self.pad = (kernel_size - 1) // 2
+
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            padding=0,
+        )
+
+    def forward(self, x):
+        # circular pad longitude
+        x = F.pad(x, pad=(self.pad, self.pad, 0, 0), mode="circular")
+        # pad latitude
+        x = F.pad(x, pad=(0, 0, self.pad, self.pad), mode=self.lat_pad_mode)
+
+        return self.conv(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(
+        self, channels, kernel_size=3, padding=1, residual_scale=1.0, post_nl=True
+    ):
+        super().__init__()
+        self.residual_scale = residual_scale
+
+        self.net = nn.Sequential(
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=kernel_size,
+                padding=padding,
+            ),
+            nn.BatchNorm2d(channels),
+            nn.GELU(),
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=kernel_size,
+                padding=padding,
+            ),
+            nn.BatchNorm2d(channels),
+        )
+        if post_nl:
+            self.act = nn.GELU()
+        else:
+            self.act = nn.Identity()
+
+    def forward(self, x):
+        return self.act(x + self.residual_scale * self.net(x))
+    
+    
+class PeriodicResidualBlock(nn.Module):
+    def __init__(
+        self, channels, kernel_size=3, residual_scale=1.0, post_nl=True, lat_pad_mode='reflect'
+    ):
+        super().__init__()
+        self.residual_scale = residual_scale
+
+        self.net = nn.Sequential(
+            MixedPeriodicConv2D(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=kernel_size,
+                lat_pad_mode=lat_pad_mode,
+            ),
+            nn.BatchNorm2d(channels),
+            nn.GELU(),
+            MixedPeriodicConv2D(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=kernel_size,
+                lat_pad_mode=lat_pad_mode,
+            ),
+            nn.BatchNorm2d(channels),
+        )
+        if post_nl:
+            self.act = nn.GELU()
+        else:
+            self.act = nn.Identity()
+
+    def forward(self, x):
+        return self.act(x + self.residual_scale * self.net(x))
--- src/tyndall/models.py
+++ src/tyndall/models.py
@@ -1,4 +1,6 @@
 from torch import nn
+import torch.nn.functional as F
+from .layers import LayerNorm2D, MixedPeriodicConv2D, ResidualBlock, PeriodicResidualBlock
 
 
 class ConvModel(nn.Module):
@@ -17,38 +19,7 @@ class ConvModel(nn.Module):
 
     def forward(self, x):
         return self.net(x)
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, channels, kernel_size=3, padding=1, residual_scale=1.0, post_nl=True):
-        super().__init__()
-        self.residual_scale = residual_scale
-
-        self.net = nn.Sequential(
-            nn.Conv2d(
-                in_channels=channels,
-                out_channels=channels,
-                kernel_size=kernel_size,
-                padding=padding,
-            ),
-            nn.BatchNorm2d(channels),
-            nn.GELU(),
-            nn.Conv2d(
-                in_channels=channels,
-                out_channels=channels,
-                kernel_size=kernel_size,
-                padding=padding,
-            ),
-            nn.BatchNorm2d(channels)
-        )
-        if post_nl:
-            self.act = nn.GELU()
-        else:
-            self.act = nn.Identity()
-
-    def forward(self, x):
-        return self.act(x + self.residual_scale * self.net(x))
-
+    
 
 class DeepResNet(nn.Module):
     def __init__(
@@ -61,7 +32,7 @@ class DeepResNet(nn.Module):
         depth=5,
         predict_residual=True,
         residual_scale=1.0,
-        post_nl=True
+        post_nl=True,
     ):
         super().__init__()
         self.predict_residual = predict_residual
@@ -85,7 +56,7 @@ class DeepResNet(nn.Module):
                     kernel_size=kernel_size,
                     padding=padding,
                     residual_scale=residual_scale,
-                    post_nl=post_nl
+                    post_nl=post_nl,
                 )
             )
 
@@ -95,6 +66,64 @@ class DeepResNet(nn.Module):
                 out_channels=out_channels,
                 kernel_size=kernel_size,
                 padding=padding,
+            )
+        )
+
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        dx = self.net(x)
+        if self.predict_residual:
+            x_last = x[:, -self.out_channels :]
+            return x_last + dx
+
+        return dx
+    
+class PeriodicResNet(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels=4,
+        hidden_channels=64,
+        kernel_size=3,
+        depth=5,
+        predict_residual=True,
+        residual_scale=1.0,
+        post_nl=True,
+        lat_pad_mode='reflect'
+    ):
+        super().__init__()
+        self.predict_residual = predict_residual
+        self.out_channels = out_channels
+
+        layers = [
+            MixedPeriodicConv2D(
+                in_channels=in_channels,
+                out_channels=hidden_channels,
+                kernel_size=kernel_size,
+                lat_pad_mode=lat_pad_mode
+            ),
+            nn.BatchNorm2d(hidden_channels),
+            nn.GELU(),
+        ]
+
+        for _ in range(depth):
+            layers.append(
+                PeriodicResidualBlock(
+                    channels=hidden_channels,
+                    kernel_size=kernel_size,
+                    residual_scale=residual_scale,
+                    post_nl=post_nl,
+                    lat_pad_mode=lat_pad_mode
+                )
+            )
+
+        layers.append(
+            MixedPeriodicConv2D(
+                in_channels=hidden_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                lat_pad_mode=lat_pad_mode,
             )
         )
 
@@ -123,7 +152,19 @@ def build_model(config):
             depth=model_cfg["depth"],
             predict_residual=model_cfg["predict_residual"],
             residual_scale=model_cfg["residual_scale"],
-            post_nl=model_cfg["post_nl"]
+            post_nl=model_cfg["post_nl"],
+        )
+    elif name == 'periodicresnet':
+        return PeriodicResNet(
+            in_channels=model_cfg["in_channels"],
+            out_channels=model_cfg["out_channels"],
+            hidden_channels=model_cfg["hidden_channels"],
+            kernel_size=model_cfg["kernel_size"],
+            depth=model_cfg["depth"],
+            predict_residual=model_cfg["predict_residual"],
+            residual_scale=model_cfg["residual_scale"],
+            post_nl=model_cfg["post_nl"],
+            lat_pad_mode=model_cfg.get("lat_pat_mode", "reflect")
         )
     else:
         raise ValueError(f"Unknown model name: {name}")
